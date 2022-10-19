ff6892bd6ee8d0951ed67e2e189dafacf1e95297
Cesar <cesarbocel222@gmail.com>
2022-10-18 19:36:58 -0600

    menu

--- PRYAVANZADA01/PRYAVANZADA01/MyForm.cpp
+++ PRYAVANZADA01/PRYAVANZADA01/MyForm.cpp
@@ -2,7 +2,6 @@
 using  namespace System;
 using namespace System::Windows::Forms;
 
-[STAThreadAttribute]
 int main(array<String^>^ args) {
 	Application::EnableVisualStyles;
 	Application::SetCompatibleTextRenderingDefault(false);
