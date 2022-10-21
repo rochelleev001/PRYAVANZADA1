dd1e0213046465c5abe3d59c9d0b0c89a4ece4a3
Cesar <cesarbocel222@gmail.com>
2022-10-21 16:58:25 -0600

    agregacion del menu

--- PRYAVANZADA01/PRYAVANZADA01/menu.h
+++ PRYAVANZADA01/PRYAVANZADA01/menu.h
@@ -34,6 +34,8 @@ namespace PRYAVANZADA01 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +50,28 @@ namespace PRYAVANZADA01 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"menu";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(44, 42);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// menu
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->Name = L"menu";
+			this->Text = L"menu";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
